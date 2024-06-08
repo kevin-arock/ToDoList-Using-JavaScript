34e4674345beefd5474f48801d42269a756d0795
kevin-arock <kevinstandlee2002@gmail.com>
2024-06-08 16:41:53 +0530

    update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function add(){
         setTimeout(()=>{
             p.classList.remove('error');
             p.classList.add('base');
-        },1100)
+        },1200)
         return;
     }
     else if(name.value === '' || date.value === '' ){
@@ -25,7 +25,7 @@ function add(){
         setTimeout(()=>{
             p.classList.remove('error');
             p.classList.add('base');
-        },1100)
+        },1200)
         return;
        
     }
@@ -42,7 +42,7 @@ function add(){
     setTimeout(()=>{
         p.classList.remove('success');
         p.classList.add('base');
-    },1100)
+    },1200)
     name.value = '';
     date.value = '';
     display();
@@ -58,7 +58,7 @@ function del(){
         setTimeout(()=>{
             p.classList.remove('delete');
             p.classList.add('base');
-        },1100);
+        },1200);
 }
 function display(){
     let show = ``;
